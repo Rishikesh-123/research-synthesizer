738a1ea3d42fc95ceae36cf0f1f214adcc14d390
Rishikesh <kumarrishikesh1418@gmail.com>
2026-05-24 23:20:43 -0400

    Adding all the imports

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from langchain_anthropic import ChatAnthropic
+from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain_community.tools import DuckDuckGoSearchResults
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import os
